5995ae2fb3835d01009fd6dbe631e17a860197c4
Kevin Simon <keviniansimon@gmail.com>
2023-07-26 14:53:04 +0200

    build: added navigation animations

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,60 @@
 'use client';
 
+import { AnimatePresence, cubicBezier, motion } from 'framer-motion';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Navigation from './Navigation';
 import Instagram from './assets/icons/Instagram';
 
+const transition = { ease: cubicBezier(0.6, 0.6, 0, 1), duration: 0.8 };
+
+const mobileNavVarients = {
+  openMenu: {
+    height: '100%',
+    transition,
+  },
+  closeMenu: {
+    height: '0%',
+    transition,
+  },
+  menuTextClose: {
+    y: '0%',
+    transition,
+  },
+  menuTextOpen: {
+    y: '-150%',
+    transition,
+  },
+  closeTextClose: {
+    y: '150%',
+    transition,
+  },
+  closeTextOpen: {
+    y: '0%',
+    transition,
+  },
+};
+
 function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const menuHandler = (event: Event) => {
+      if (!menuRef.current) {
+        return;
+      }
+
+      if (!menuRef.current.contains(event.target as HTMLDivElement)) {
+        setIsNavOpen(false);
+      }
+    };
+
+    document.addEventListener('click', menuHandler, true);
+
+    return () => document.removeEventListener('click', menuHandler);
+  }, []);
 
   const handleNavClick = () => {
     setIsNavOpen((isOpen) => !isOpen);
@@ -26,16 +74,41 @@ function Header() {
             Kels Illustrates
           </p>
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center" ref={menuRef}>
+          <AnimatePresence>
+            <motion.div
+              className={`md:static absolute overflow-hidden left-0 right-0 w-full px-10 top-full z-[9999] bg-white md:h-full`}
+              variants={mobileNavVarients}
+              initial={'closeMenu'}
+              exit={'closeMenu'}
+              animate={isNavOpen ? 'openMenu' : 'closeMenu'}
+              onClick={handleNavClick}
+            >
+              <Navigation />
+            </motion.div>
+          </AnimatePresence>
           <div
-            className={`md:static absolute overflow-hidden left-0 right-0 w-full px-10 top-full z-[9999] bg-white md:h-full ${
-              isNavOpen ? 'py-7 h-full' : 'py-0 h-0'
-            }`}
+            className="md:hidden relative overflow-hidden w-12 h-8"
+            onClick={handleNavClick}
           >
-            <Navigation />
-          </div>
-          <div className="md:hidden" onClick={handleNavClick}>
-            <p className="font-Josefin font-light text-lg">Menu</p>
+            <motion.p
+              className="font-Josefin font-light text-lg absolute top-0 right-0 left-0 bottom-0"
+              variants={mobileNavVarients}
+              initial={'menuTextClose'}
+              exit={'menuTextOpen'}
+              animate={isNavOpen ? 'menuTextOpen' : 'menuTextClose'}
+            >
+              Menu
+            </motion.p>
+            <motion.p
+              className="font-Josefin font-light text-lg absolute top-0 right-0 left-0 bottom-0"
+              variants={mobileNavVarients}
+              initial={'closeTextClose'}
+              exit={'closeTextOpen'}
+              animate={isNavOpen ? 'closeTextOpen' : 'closeTextClose'}
+            >
+              Close
+            </motion.p>
           </div>
           <a
             href="https://instagram.com/kels_illustrates"
